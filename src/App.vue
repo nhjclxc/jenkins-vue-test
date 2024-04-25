322f85db165904ab401abb0df45a78e4f0ea35fd
LuoXianchao <nhjclxc@163.com>
2024-04-25 21:53:48 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+
+    <button @click="requestTest">{{ env }}</button>
+    <img alt="Vue logo" src="./assets/logo.png" >
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     你好世界
   </div>
@@ -9,11 +11,45 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+//安装axios：npm i axios
+
+// 使用axios
+// eslint-disable-next-line no-unused-vars
+
+// eslint-disable-next-line no-unused-vars
+import request from '@/request'
+
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      'env': process.env.VUE_APP_TITLE
+    }
+  },
+  methods: {
+    requestTest(){
+      console.log('requestTest')
+      request({
+        method: 'get',
+        // 没有代理的时候写的是5000，但是现在有代理了，直接写8080
+        // url: 'http://127.0.0.1:5000/students'
+        url: '/testCon/test1'
+      }).then(function (response) {
+        console.log(response);
+        console.log(response.data)
+      }).catch(function (error) {
+        console.log('error', error);
+      }).finally(response => {
+        console.log('finally', response)
+      });
+    }
+  },
+  mounted() {
+    this.requestTest()
+  },
 }
 </script>
 
@@ -27,3 +63,12 @@ export default {
   margin-top: 60px;
 }
 </style>
+<!--
+
+
+//    "build:test": "vue-cli-service build --mode test",
+
+//    "serve": "SET NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service serve",
+//    "build": "SET NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service build",
+//    "build:test": "SET NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service build --mode test",
+-->
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint校验
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      // https://cli.vuejs.org/zh/config/
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_BASE_SERVE,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
